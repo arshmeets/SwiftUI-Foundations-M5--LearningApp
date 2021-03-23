26af11ceaf6d689dfa216d6d36bc591d88468287
Arshmeet Sodhi <arshmeetsodhi@gmail.com>
2021-03-23 11:06:42 -0400

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Arshmeet Sodhi on 3/23/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
