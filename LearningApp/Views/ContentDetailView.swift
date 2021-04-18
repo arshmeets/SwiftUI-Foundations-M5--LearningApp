3419567f1b70a5af35cc7ab701f8d4afece3478a
Arshmeet Sodhi <arshmeetsodhi@gmail.com>
2021-04-18 18:45:36 -0400

    - Add the UI elements for the next lesson (Green Rectangle with Padding)

--- LearningApp/Views/ContentDetailView.swift
+++ LearningApp/Views/ContentDetailView.swift
@@ -28,13 +28,28 @@ struct ContentDetailView: View {
             // Show the next lesson button, only if there is a next lesson
             if model.hasNextLesson() {
                 Button(action: {
+                    
                     model.nextLesson()
                     
                 }, label: {
-                    Text("Next Lesson")
+                    ZStack {
+                        
+                        Rectangle()
+                            .frame(height:48)
+                            .foregroundColor(.green)
+                            .cornerRadius(10)
+                            .shadow(radius:5)
+                        
+                        Text("Next Lesson: \(model.currentModule!.content.lessons[model.currentLessonIndex + 1].title)")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                    
+                    
                 })
             }
         }
+            .padding(.horizontal)
         
     }
 }
